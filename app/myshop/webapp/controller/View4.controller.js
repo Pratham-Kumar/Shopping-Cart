ba155d7c504a6b36b43528ed2ac69a93f11b3722
Pratham Kumar <kumarp@terralink-global.com>
2023-11-20 12:31:30 +0000

    New Changes

--- app/myshop/webapp/controller/View4.controller.js
+++ app/myshop/webapp/controller/View4.controller.js
@@ -5,22 +5,25 @@ sap.ui.define(
  
         return Controller.extend("com.sap.myshop.controller.View4", {
             onInit: function () {
-    
+   
             },
  
-            onConfirmOrder: function () {
+            confirmOrder: function () {
+                debugger;
                 const oModel = this.getOwnerComponent().getModel();
                
                 const oName = this.getView().byId("name").getValue();
                 const oAddress = this.getView().byId("add").getValue();
                 const oMobile = this.getView().byId("mob").getValue();
                 const oPin_code = this.getView().byId("pin").getValue();
- 
+               
+               
                 let myData = {
-                    "name" : oName,
-                    "address" : oAddress,
-                    "mobile" : oMobile,
-                    "pin_code" : oPin_code
+                    "ID": 2,
+                    "name" : "Abhiiiiiii",
+                    "address" : "qqqqqqq",
+                    "mobile" : 3434343456,
+                    "pin_code" : 201206
                 }
  
                 oModel.create("/Customers", myData, {
@@ -35,13 +38,11 @@ sap.ui.define(
             },
  
             onCancelOrder: function () {
-               
-            },
- 
-            validateFormData: function (oFormData) {
-                // Add your validation logic here
-                return !!oFormData.name && !!oFormData.mobile && !!oFormData.address && !!oFormData.pin_code;
-            },
+               const oName = this.getView().byId("name").setValue();
+                const oAddress = this.getView().byId("add").setValue();
+                const oMobile = this.getView().byId("mob").setValue();
+                const oPin_code = this.getView().byId("pin").setValue();
+            }
         });
     }
 );
